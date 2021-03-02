c1c3ef4a2477a8bb57cdc71f886c806727eaa752
gugus2000 <gugus2000@protonmail.com>
2021-03-02 21:43:58 +0100

    typo fixes

--- resources/js/style.js
+++ resources/js/style.js
@@ -1,4 +1,4 @@
-document.getElementById('main').height=(CONFIG['game']['border'][1]['max']-CONFIG['game']['border'][1]['min'])+'px';
-document.getElementById('game').width=(CONFIG['game']['border'][0]['max']-CONFIG['game']['border'][0]['min'])+'px';
+document.getElementById('main').height=(CONFIG['game']['border'][1]['max']-CONFIG['game']['border'][1]['min']);
+document.getElementById('game').width=(CONFIG['game']['border'][0]['max']-CONFIG['game']['border'][0]['min']);
 document.getElementsByTagName('title')[0].removeChild(document.getElementsByTagName('title')[0].childNodes[0]);
 document.getElementsByTagName('title')[0].appendChild(document.createTextNode(LANG['page']['title']));
