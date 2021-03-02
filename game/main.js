17891cc44c928040990cab185910496f247ddea4
gugus2000 <gugus2000@protonmail.com>
2021-03-02 22:01:16 +0100

    typo fix

--- game/main.js
+++ game/main.js
@@ -39,7 +39,6 @@ class Game
 		if (GLOBALS['keys_pressed'][CONFIG['game']['shortcut']['pause']]) // Pause the game when p is pressed
 		{
 			console.log('a');
-		}
 			this.is_running = false;
 		}
 
